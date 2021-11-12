f20d4f88eebfc9722bbd2026ac0d7c8d3e64bec5
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-11-12 09:12:43 +0000

    Test our app instead of generic example tests
    This commit modifies the functional tests to test our app, instead of
    the generic example tests that were there before.

--- functional-tests/tests/step_implementation.js
+++ functional-tests/tests/step_implementation.js
@@ -43,48 +43,11 @@ gauge.customScreenshotWriter = async function () {
     return path.basename(screenshotFilePath);
 };
 
-step("Add task <item>", async (item) => {
-    await write(item, into(textBox("What needs to be done?")));
-    await press('Enter');
+step("Open available pets application", async function () {
+    await goto("https://available-pets-consumer.herokuapp.com");
 });
 
-step("View <type> tasks", async function (type) {
-    await click(link(type));
-});
-
-step("Complete tasks <table>", async function (table) {
-    for (var row of table.rows) {
-        await click(checkBox(toLeftOf(row.cells[0])));
-    }
-});
-
-step("Clear all tasks", async function () {
-    await evaluate(() => localStorage.clear());
-});
-
-step("Open todo application", async function () {
-    await goto("todo.taiko.dev");
-});
-
-step("Must not have <table>", async function (table) {
-    for (var row of table.rows) {
-        assert.ok(!await text(row.cells[0]).exists(0, 0));
-    }
-});
 
 step("Must display <message>", async function (message) {
     assert.ok(await text(message).exists(0, 0));
-});
-
-step("Add tasks <table>", async function (table) {
-    for (var row of table.rows) {
-        await write(row.cells[0]);
-        await press('Enter');
-    }
-});
-
-step("Must have <table>", async function (table) {
-    for (var row of table.rows) {
-        assert.ok(await text(row.cells[0]).exists());
-    }
 });
