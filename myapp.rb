8a734025c417e0e1321187c8df075aac5558614a
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-11-13 07:59:03 +0000

    Use Prism mock instead of real API
    One interesting thing about this is that when we used Prism to mock the
    API call, it instantly flagged that we were missing the required
    authorization header in our request (we ran Prism with the `--errors`
    flag which is something we should always do, to discover errors like
    this).
    
    This switch to use the Prism mock simply involved changing the
    `PETSTORE_URL` environment variable to point to Prism, i.e.
    `http://127.0.0.1:4010/` when running locally, and setting up a
    [Stoplight hosted Prism mock][1] when running on a Review App, i.e.
    `https://stoplight.io/mocks/agilepathway/java-openapi-provider/84330/`.
    
    In a later commit we'll setup the URL to be dynamic based on the Git
    branch, so that we can use an individual Prism mock server for every
    branch.
    
    [1]: https://meta.stoplight.io/docs/platform/ZG9jOjIwMTI4MA-working-with-mock-servers

--- myapp.rb
+++ myapp.rb
@@ -7,7 +7,22 @@ get('/') do
 end
 
 def pets
-  url = "#{ENV['PETSTORE_URL']}pet/findByStatus?status=available"
-  response = Net::HTTP.get(URI(url))
-  JSON.parse(response)
+  get_json "#{petstore_url}pet/findByStatus?status=available"
+end
+
+def get_json(url)
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true if uri.instance_of? URI::HTTPS
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request['Accept'] = 'application/json'
+  request['authorization'] = 'Bearer YOUR_ACCESS_TOKEN'
+  response = http.request(request)
+  JSON.parse(response.body)
+end
+
+def petstore_url
+  petstore_url = ENV['PETSTORE_URL']
+  puts "Petstore URL is #{petstore_url}"
+  petstore_url
 end
